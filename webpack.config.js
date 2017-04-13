783eced189353b60d74d6ed1f4aa2f0c097d3c3e
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-04-13 20:12:22 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+let externals = _externals();
+
+module.exports = {
+    context: path.resolve(__dirname, './app'),
+    entry: {
+        app: './entry.js',
+    },
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    /*resolve: {
+        extensions: ['', '.js']
+    },*/
+    // externals: externals,
+    node: {
+        console: true,
+        global: true,
+        process: true,
+        Buffer: true,
+        __filename: true,
+        __dirname: true,
+        setImmediate: true
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'stage-0']
+            },
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
+
+function _externals() {
+    let manifest = require('./package.json');
+    let dependencies = manifest.dependencies;
+    let externals = {};
+    for (let p in dependencies) {
+        externals[p] = 'commonjs ' + p;
+    }
+    return externals;
+}
