186f62fb1f0fff605f854e7e114164ee5d3e3b5b
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-05-17 18:07:43 +0800

    aaa

--- app/main/test.js
+++ app/main/test.js
@@ -1,9 +1,12 @@
 var request =require("request")
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 request({
   method:"GET",
   url:'http://adpays.net/v.php?user=4297',
-  proxy:"http://127.0.0.1:10005",
+  // proxy:"http://127.0.0.1:1080/",
   followAllRedirects:true,
   headers:{
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -14,12 +17,27 @@ request({
     "authority":"hidemy.name",
     "cache-control":"max-age=0",
     // "Connection":"keep-alive",
-    'Cookie':"__cfduid=d76cafce38bd01b3b60041af7c7da412c1491010798; PHPSESSID=gflj3dti5bhj6rjol2130iheo7; TawkConnectionTime=0; __tawkuuid=e::adpays.net::nlF8BgfwL9/xShZQs7lP+IDLjoC4VJNJFauv4HokXlnn3l1uvpkAud9hlBKxHIXO::2; Tawk_5703f9be5c96cca60a9d16c2=vs3.tawk.to::0; ptp=1494564903; _ga=GA1.2.518476455.1494559785; _gid=GA1.2.1591825687.1494565013; _gat=1",
+    // 'Cookie':"__cfduid=d76cafce38bd01b3b60041af7c7da412c1491010798; PHPSESSID=gflj3dti5bhj6rjol2130iheo7; TawkConnectionTime=0; __tawkuuid=e::adpays.net::nlF8BgfwL9/xShZQs7lP+IDLjoC4VJNJFauv4HokXlnn3l1uvpkAud9hlBKxHIXO::2; Tawk_5703f9be5c96cca60a9d16c2=vs3.tawk.to::0; ptp=1494564903; _ga=GA1.2.518476455.1494559785; _gid=GA1.2.1591825687.1494565013; _gat=1",
   }
 },function(error, response, body){
   if(error){
     console.log("error"+error)
   }else{
-    console.log('bingo')
+    const {window} = new JSDOM(body, {
+      // url: "http://adpays.net/v.php?user=4297",
+      // referrer: "http://adpays.net/v.php?user=4297",
+      // contentType: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      // userAgent: "Mozilla/8.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
+      // includeNodeLocations: true,
+      runScripts: "dangerously"
+    });
+    debugger
+console.log(window.document)
+
+    // setTimeout(()=>{
+    //   var tem =require('./tem.js')
+    // },20000)
+
+    // console.log(dom.window.innerWidth)
   }
 })
