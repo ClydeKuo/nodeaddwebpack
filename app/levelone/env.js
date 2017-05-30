9f7a7c042fc099501fd315efcba25bdc548fc187
ClydeKuo <clydekuo6@gmail.com>
2017-05-30 22:30:57 +0800

    asd

--- app/levelone/env.js
+++ app/levelone/env.js
@@ -1,6 +1,6 @@
 var proxyIp="http://127.0.0.1:1080"
 if (process.platform == 'linux') {
     console.log('This platform is linux:' + (process.platform == 'linux'));
-    proxy = ""
+    proxyIp = ""
 }
 module.exports=proxyIp
