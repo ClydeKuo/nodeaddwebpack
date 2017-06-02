2ab2de53749d676db9bebc492103fb3ddba357b3
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-06-02 14:39:33 +0800

    添加ptp

---
+++ app/test/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var P2PSpider = require('../lib');
+
+var p2p = P2PSpider({
+    nodesMaxSize: 1,   // be careful
+    maxConnections: 1, // be careful
+    timeout: 5000
+});
+
+p2p.ignore(function (infohash, rinfo, callback) {
+    // false => always to download the metadata even though the metadata is exists.
+    var theInfohashIsExistsInDatabase = false;
+    callback(theInfohashIsExistsInDatabase);
+});
+
+p2p.on('metadata', function (metadata) {
+console.log('----------------------------')
+onsole.log(new date())
+    console.log(metadata);
+});
+
+p2p.listen(6881, '0.0.0.0');
