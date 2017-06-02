ebf453dbbeb9583cd081f6971b4ad2c6507a1d5c
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-06-02 16:41:28 +0800

    萨达阿萨

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 var mongourl = 'mongodb://clyde:asdqwe123@list-shard-00-00-si9p2.mongodb.net:27017,list-shard-00-01-si9p2.mongodb.net:27017,list-shard-00-02-si9p2.mongodb.net:27017/p2p?ssl=true&replicaSet=list-shard-0&authSource=admin'
 // 获得db对象
-let db = mongoose.connection;
+var db = mongoose.connection;
 // 各种事件
 // connection的事件列表可查看:http://mongoosejs.com/docs/api.html#connection_Connection
 // 或 ./node_modules/mongoose/lib/connection.js#Connection()
