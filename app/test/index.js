86245c656f9886500471def92f8d443006c766df
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-06-03 11:45:41 +0800

    检查重复数据

--- app/test/index.js
+++ app/test/index.js
@@ -24,27 +24,38 @@ function saveData(data){
       console.log('meow');
     })
 }
+
 function change(value){
     if(value/1024>=1&&value/1024<1024){
-        return value/1024+'kb'
+        return Math.round(value/1024)+'kb'
     }else if(value/1048576>=1&&value/1048576<1024){
-        return value/1048576+"mb"
+        return Math.round(value/1048576)+"mb"
     }else if(value/1073741824>=1&&value/1073741824<1024){
-        return value/1073741824 +'gb'
+        return Math.round(value/1073741824) +'gb'
+    }else if(value/1099511627776>=1&&value/1099511627776<1024){
+        return Math.round(value/1099511627776) +'tb'
     }else{
-        return value/1099511627776 +'tb'
+        return 'NaN: '+value
     }
 }
 p2p.on('metadata', function (metadata) {
-    console.log('----------------------------')
-    console.log(new Date())
-    var data={infohash:metadata.infohash,
-            magnet:metadata.magnet,
-            name:metadata.info.name.toString(),
-            length:change(metadata.info.length)
-    }
-    saveData(data)
-    console.log(data);
+    tdb.find({infohash:infohash}).then(function(res){
+        if(res.length>0){
+            console.log('----------------------------')
+            console.log(new Date())
+            var data={infohash:metadata.infohash,
+                    magnet:metadata.magnet,
+                    name:metadata.info.name.toString(),
+                    length:change(metadata.info.length)
+            }
+            saveData(data)
+            console.log(data);
+        }else{
+            console.log('repeated')
+        }
+    },function(err){
+        console.log('err: '+err)
+    })
 });
 
 p2p.listen(6881, '0.0.0.0');
--- app/test/test.js
+++ app/test/test.js
@@ -1,4 +1,12 @@
 require('../lib/db')
-tdb.find({port:51413}).exec(function(err,list){
-    debugger
+// tdb.find({port:51413}).exec(function(err,list){
+//     debugger
+// })
+function check(infohash){
+    return tdb.find({infohash:infohash})
+}
+check('ad01e26fff09b01b67043b9616070e38857848').then(function(res){
+    console.log(res)
+},function(err){
+    console.log('err:  '+err)
 })
